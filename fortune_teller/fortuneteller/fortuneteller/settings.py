84f557749bcb77eea0b3612010e76d5c37ae37ad
Derek <112670195+dsmdex@users.noreply.github.com>
2025-03-12 23:54:38 -0700

    new django app

--- fortune_teller/fortuneteller/fortuneteller/settings.py
+++ fortune_teller/fortuneteller/fortuneteller/settings.py
@@ -118,8 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Ensure Django looks for static files
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- fortune_teller/fortuneteller/randomfortune/urls.py
+++ fortune_teller/fortuneteller/randomfortune/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.fortune, name="fortune"),
+    path('', views.fortune, name="randomfortune"),
 ]
